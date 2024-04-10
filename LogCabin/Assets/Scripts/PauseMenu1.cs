eaee436eab8386a41042a4662220080ab406040e
yeetmyneet <omar1207f@gmail.com>
2024-04-10 09:22:39 -0700

    Added new scripts

---
+++ LogCabin/Assets/Scripts/PauseMenu1.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+	[SerializeField] Canvas pauseMenu;
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+			pauseMenu.enabled = true;
+		}
+		else if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 0)
+		{
+			Resume();
+		}
+	}
+
+	public void Resume()
+	{
+		Time.timeScale = 1;
+		pauseMenu.enabled = false;
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+	public void LoadMainMenu()
+	{
+		Time.timeScale = 1;
+		SceneManager.LoadScene("Main Menu");
+	}
+
+	public void LoadPauseMenu()
+	{
+		Time.timeScale = 0;
+		pauseMenu.enabled = true;
+	}
+}
