6d17e95bfb16374343fc943d9ac2543a52a6c485
polystation1207 <25.omar.fierro@ksd.org>
2024-04-12 18:01:39 -0700

    Started on footsteps script

--- LogCabin/Assets/Scripts/FootstepSound.cs
+++ LogCabin/Assets/Scripts/FootstepSound.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class FootstepSound : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    public AudioSource footstepsSound;
+
+    [SerializeField] AudioClip wood;
+    [SerializeField] AudioClip snow;
+
+    RaycastHit hit;
+    public Transform RayStart;
+    public float range;
+    public LayerMask layerMask;
+
+    private void Footstep()
     {
-        
+        if(Physics.Raycast(RayStart.position, RayStart.transform.up * -1, out hit, range, layerMask))
+        {
+            if (hit.collider.CompareTag("Wood Floor"))
+            {
+                PlayFootstepSoundL()
+            }
+        }
     }
 }
