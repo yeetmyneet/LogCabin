ab2b4d787de4ea2a4fdc761516f3b0b8d8d1f170
polystation1207 <25.omar.fierro@ksd.org>
2024-05-08 14:00:54 -0700

    added level exit, sounds and fixed UI

---
+++ LogCabin/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    public FadeOutDeath fadeScript;
+    public AudioSource truckSounds;
+    public AudioClip truckStart;
+    [SerializeField] bool isCabin = false;
+    [SerializeField] bool isGasStation = false;
+    [SerializeField] float levelLoadDelay = 7f;
+    [SerializeField] FirstPersonController MOVE;
+    [SerializeField] FootstepSound stepStomp;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Truck")
+        {
+            fadeScript.StartCoroutine(fadeScript.FadeToBlack());
+            StartCoroutine(LoadNextLevel());
+            truckSounds.PlayOneShot(truckStart);
+            MOVE.enabled = false;
+            stepStomp.enabled = false;
+        }
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (isCabin == true)
+        {
+            SceneManager.LoadScene("GasStation");
+        }
+        if (isGasStation == true)
+        {
+            SceneManager.LoadScene("HuntingGrounds");
+        }
+    }
+
+
+}
