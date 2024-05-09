68de6b9139901dd2b90ee8f8ca5ec233f00b8086
Battlebushost <62022286+quest666scary@users.noreply.github.com>
2024-05-09 09:51:43 -0700

    added checks for the footstep sound script

--- LogCabin/Assets/Scripts/FootstepSound.cs
+++ LogCabin/Assets/Scripts/FootstepSound.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class FootstepSound : MonoBehaviour
 {
+    #region Inspector References
     public float stepRate = 0.5f;
+    public string woodSurfaceTag = "Wood Floor";
+    public string snowSurfaceTag = "Snow Floor";
+    public string concreteSurfaceTag = "Concrete Floor";
     public float stepCooldown;
     public AudioSource source;
-    public AudioClip footstep;
-
+    public AudioClip woodFootstep;
+    public AudioClip snowFootstep;
+    public AudioClip concreteFootstep;
+#endregion Inspector References
     private void Update()
     {
         stepCooldown -= Time.deltaTime;
         if ((Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) && stepCooldown < 0f)
         {
             source.pitch = 1f + Random.Range(-0.2f, 0.2f);
-            source.PlayOneShot(footstep, 0.9f);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, 1f))
+            {
+                if (hit.collider.CompareTag(woodSurfaceTag))
+                {
+                    source.PlayOneShot(woodFootstep, 0.8f);
+                }
+                if (hit.collider.CompareTag(snowSurfaceTag))
+                {
+                    source.PlayOneShot(snowFootstep, 0.8f);
+                }
+                if (hit.collider.CompareTag(concreteSurfaceTag))
+                {
+                    source.PlayOneShot(concreteFootstep, 0.8f);
+                }
+            }
             stepCooldown = stepRate;
         }
     }
