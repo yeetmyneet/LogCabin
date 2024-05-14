5fd058658b9e81b6a8b17581f4c59e98592baaad
polystation1207 <25.omar.fierro@ksd.org>
2024-05-13 22:14:08 -0700

    Added muzzle flash to shotgun

--- LogCabin/Assets/Scripts/RaycastShoot.cs
+++ LogCabin/Assets/Scripts/RaycastShoot.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class RaycastShoot : MonoBehaviour
 {
+    public Transform muzzle;
+    public GameObject muzzleFlash;
     public EnemyController enemyController;
     [SerializeField] AudioClip gunshot;
     [SerializeField] Animator gunAnim;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
-        {
+        {  
             gunAnim.SetTrigger("shot");
-            Debug.Log("shooting");
+
+            if (muzzleFlash != null)
+            {
+                Instantiate(muzzleFlash, muzzle.position, Quaternion.identity); ;
+            }
+
             RaycastHit hit;
             Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
             if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider != null)
